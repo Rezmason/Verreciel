a3d8a4f7ef7d1c79ba7df9dbcd9217e2f13b4bf4
Rezmason <jeremysachs@rezmason.net>
2019-08-28 00:57:30 -0700

    Recipes are now constructed from RecipeData.

--- desktop/sources/scripts/collections/recipes.js
+++ desktop/sources/scripts/collections/recipes.js
@@ -1,109 +1,89 @@
 //  Created by Devine Lu Linvega.
 //  Copyright © 2017 XXIIVV. All rights reserved.
 
+const recipeData = [
+  // Records
+  {
+    ingredientIDs: ['record1', 'record2'],
+    resultID: 'record4',
+    like: false
+  },
+  {
+    ingredientIDs: ['record1', 'record3'],
+    resultID: 'record4',
+    like: false
+  },
+  {
+    ingredientIDs: ['record2', 'record3'],
+    resultID: 'record4',
+    like: false
+  },
+
+  // Keys
+  {
+    ingredientIDs: ['valenPortalFragment1', 'valenPortalFragment2'],
+    resultID: 'valenPortalKey',
+    like: false
+  },
+  {
+    ingredientIDs: ['usulPortalFragment1', 'usulPortalFragment2'],
+    resultID: 'usulPortalKey',
+    like: false
+  },
+  {
+    ingredientIDs: ['valenPortalKey', 'usulPortalKey'],
+    resultID: 'endPortalKeyFragment1',
+    like: false
+  },
+  {
+    ingredientIDs: ['loiqePortalKey', 'senniPortalKey'],
+    resultID: 'endPortalKeyFragment2',
+    like: false
+  },
+  {
+    ingredientIDs: ['endPortalKeyFragment1', 'endPortalKeyFragment2'],
+    resultID: 'endPortalKey',
+    like: false
+  },
+
+  // Maps
+  {
+    ingredientIDs: ['map1', 'map2'],
+    resultID: 'map3',
+    like: false
+  },
+
+  // Currencies
+  {
+    ingredientIDs: ['currency1', 'currency2'],
+    resultID: 'currency4',
+    like: true
+  },
+  {
+    ingredientIDs: ['currency2', 'currency3'],
+    resultID: 'currency5',
+    like: true
+  },
+  {
+    ingredientIDs: ['currency4', 'currency5'],
+    resultID: 'currency6',
+    like: true
+  }
+]
+
 class Recipes {
   constructor () {
     // assertArgs(arguments, 0);
     this.horadric = []
 
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.record1, verreciel.items.record2],
-        verreciel.items.record4
-      )
-    )
-
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.record1, verreciel.items.record3],
-        verreciel.items.record4
-      )
-    )
-
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.record2, verreciel.items.record3],
-        verreciel.items.record4
-      )
-    )
-
-    // Keys
-
-    this.horadric.push(
-      new Recipe(
-        [
-          verreciel.items.valenPortalFragment1,
-          verreciel.items.valenPortalFragment2
-        ],
-        verreciel.items.valenPortalKey
-      )
-    )
-
-    this.horadric.push(
-      new Recipe(
-        [
-          verreciel.items.usulPortalFragment1,
-          verreciel.items.usulPortalFragment2
-        ],
-        verreciel.items.usulPortalKey
-      )
-    )
-
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.valenPortalKey, verreciel.items.usulPortalKey],
-        verreciel.items.endPortalKeyFragment1
-      )
-    )
-
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.loiqePortalKey, verreciel.items.senniPortalKey],
-        verreciel.items.endPortalKeyFragment2
-      )
-    )
-
-    this.horadric.push(
-      new Recipe(
-        [
-          verreciel.items.endPortalKeyFragment1,
-          verreciel.items.endPortalKeyFragment2
-        ],
-        verreciel.items.endPortalKey
-      )
-    )
-
-    // Maps
-
-    this.horadric.push(
-      new Recipe(
-        [
-          verreciel.items.map1,
-          verreciel.items.map2
-        ],
-        verreciel.items.map3
-      )
-    )
-
-    // Currencies
-
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.currency1, verreciel.items.currency2],
-        Item.like(verreciel.items.currency4)
-      )
-    )
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.currency2, verreciel.items.currency3],
-        Item.like(verreciel.items.currency5)
-      )
-    )
-    this.horadric.push(
-      new Recipe(
-        [verreciel.items.currency4, verreciel.items.currency5],
-        Item.like(verreciel.items.currency6)
-      )
-    )
+    recipeData.forEach(data => {
+      const {ingredientIDs, resultID, like} = data
+      const ingredients = ingredientIDs.map(id => verreciel.items[id])
+      const result = verreciel.items[resultID]
+      this.horadric.push(new Recipe(
+        ingredients,
+        like ? Item.like(result) : result
+      ))
+    })
   }
 }
